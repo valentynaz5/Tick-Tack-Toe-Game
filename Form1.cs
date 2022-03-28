56815a6273450708a9bfaae5a29718bd0a009e5b
valentynaz5 <98236031+valentynaz5@users.noreply.github.com>
2022-03-28 09:40:44 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tic_Tac_Toe
+{
+    public partial class MainForm : Form
+    {
+        //int x = 12, y = 12;
+        Button[,] buttons = new Button[3, 3];
+        int player;
+        Label label1 = new Label();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.label1.Location = new Point(631, 13);
+            this.label1.Size = new Size(35, 13);
+
+            this.Height = 700;
+            this.Width = 900;
+            player = 1;
+            label1.Text = "Текущий ход: Игрок 1";
+            for (int i = 0; i < buttons.Length / 3; i++)
+            {
+                for (int j = 0; j < buttons.Length / 3; j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Size = new Size(200, 200);
+                }
+            }
+            setButtons();
+        }
+
+        private void setButtons()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Location = new Point(12 + 206 * j, 12 + 206 * i);
+                    buttons[i, j].Click += button1_Click;
+                    buttons[i, j].Font = new Font(new FontFamily("Microsoft Sans Serif"), 138);
+                    buttons[i, j].Text = "";
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (player)
+            {
+                case 1:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "x");
+                    player = 0;
+                    label1.Text = "Текущий ход: Игрок 2";
+                    break;
+                case 0:
+                    sender.GetType().GetProperty("Text").SetValue(sender, "o");
+                    player = 1;
+                    label1.Text = "Текущий ход: Игрок 1";
+                    break;
+            }
+            sender.GetType().GetProperty("Enabled").SetValue(sender, false);
+            checkWin();
+        }
+        private void checkWin()
+        {
+
+            if (buttons[0, 0].Text == buttons[0, 1].Text && buttons[0, 1].Text == buttons[0, 2].Text)
+            {
+
+                if (buttons[0, 0].Text != "")
+                {
+                    MessageBox.Show("Вы победили!");
+                    return;
+                }
+            }
+            if (buttons[1, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[1, 2].Text)
+            {
+                if (buttons[1, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[2, 0].Text == buttons[2, 1].Text && buttons[2, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 0].Text == buttons[1, 0].Text && buttons[1, 0].Text == buttons[2, 0].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 1].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 1].Text)
+            {
+                if (buttons[0, 1].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 2].Text == buttons[1, 2].Text && buttons[1, 2].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 2].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[0, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[2, 2].Text)
+            {
+                if (buttons[0, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+            if (buttons[2, 0].Text == buttons[1, 1].Text && buttons[1, 1].Text == buttons[0, 2].Text)
+            {
+                if (buttons[2, 0].Text != "")
+                    MessageBox.Show("Вы победили!");
+            }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    buttons[i, j].Text = "";
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+    }
+}
